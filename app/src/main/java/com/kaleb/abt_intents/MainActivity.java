66a4f738d5be2a0b1565ebea88ffd2cb92732535
kaleb <skalebsmulu@gmail.com>
2022-12-12 14:22:30 +0300

    Initial commit

---
+++ app/src/main/java/com/kaleb/abt_intents/MainActivity.java
@@ -0,0 +1,75 @@
+package com.kaleb.abt_intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText websiteEditText;
+    private EditText locationEditText;
+    private EditText shareTextEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        websiteEditText = findViewById(R.id.website_edittext);
+        locationEditText = findViewById(R.id.location_edittext);
+        shareTextEditText = findViewById(R.id.sharetext_edittext);
+
+    }
+
+    public void openWebsite(View view) {
+        // Get the URL text.
+        String url = websiteEditText.getText().toString();
+
+        // Parse the URI and create the intent.
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // Find an activity to hand the intent and start that activity.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+
+    }
+
+
+    public  void openLocation(View view){
+        String loc = locationEditText.getText().toString();
+
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+
+    }
+
+
+    public void shareText(View view) {
+        String txt = shareTextEditText.getText().toString();
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle("Share this text with: ")
+                .setText( "' " + txt + " ' " + "Intent App From Kaleb Mulugeta https://CalebMulugeta.com ")
+                .startChooser();
+    }
+
+
+
+}
